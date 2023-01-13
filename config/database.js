f79b0ca06e9858f32444d345fc64bc02f1a0d07b
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-01-13 09:46:40 +0700

    setup .env for securing connection to db

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = new Sequelize({
-  host: "localhost",
-  username: "root",
-  password: "bagas371006",
-  database: "sequelize_db",
-  dialect: "mysql",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  dialect: process.env.DB_DIALECT,
 });
 
 export default db;
